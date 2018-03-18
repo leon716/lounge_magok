c1eb7570f36743253ab9568f8769d62dd93e8a97
Jisu <thefluxusian@gmail.com>
2018-03-18 19:22:54 +0900

    temp

--- 5일차/ALLERGY.java
+++ 5일차/ALLERGY.java
@@ -5,15 +5,18 @@ public class ALLERGY {
 	static int min;
 	static int N;
 	static int M;
+	static boolean isVisited[];
+
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 
 		int T = sc.nextInt();
 		for (int t = 0; t < T; t++) {
-			min = Integer.MAX_VALUE ;
+			min = Integer.MAX_VALUE;
 			N = sc.nextInt();
 			M = sc.nextInt();
+			isVisited = new boolean[M];
 			sc.nextLine();
 			String[] names = sc.nextLine().split(" ");
 			int[] foods = new int[M];
@@ -22,52 +25,60 @@ public class ALLERGY {
 				int canEatBit = bitMaskBuilder(names, line);
 				foods[i] = canEatBit;
 			}
-			getMin(foods, 0, 1 , foods[0]);
+			for (int i = 0; i < M; i++) {
+				isVisited[i] = true;
+				getMin(foods, 0, foods[i]);
+				isVisited[i] = false;
+			}
 			System.out.println(min);
 		}
 
 	}
-	
-	public static void getMin(int[] foods, int foodCount, int visitBit, int allergyBit) {
-		if(foodCount>min) {
+
+	public static void getMin(int[] foods, int foodCount, int allergyBit) {
+		if (foodCount > min) {
 			return;
 		}
-		if(allergyBit==0) {
-			if(foodCount<min) {
+		if (allergyBit == 0) {
+			if (foodCount < min) {
 				min = foodCount;
 			}
 			return;
 		}
-		if(visitBit==(1<<N)-1) {
-			return;
-		}
-		for(int i =0; i < M ; i++ ) {
-			if((1<<i^visitBit)!=0) {
+		
+		for (int i = 0; i < M; i++) {
+			if (isVisited[i]) {
 				continue;
 			}
-//			System.out.println(allergyBit&foods[i]);
-			getMin(foods, ++foodCount, visitBit&1<<i , allergyBit&foods[i] );
+			isVisited[i] = true;
+			getMin(foods, ++foodCount, allergyBit & foods[i]);
+			isVisited[i] = false;
+
+
 		}
+		// System.out.println(allergyBit&foods[i]);
 	}
 
+	
+
 	public static int bitMaskBuilder(String[] names, String line) {
-		int result = (1 << N) - 1;
-//		System.out.println(result);
+		int result = (1 << N) - 1; // 사람 수만큼 1111... 생성
+		// System.out.println(result);
 		String[] temp = line.split(" ");
-//		for(int i = 0 ; i < temp.length ; i++ ) {
-//			System.out.print(temp[i]+" ");
-//		}
+		// for(int i = 0 ; i < temp.length ; i++ ) {
+		// System.out.print(temp[i]+" ");
+		// }
 		int numberOfNames = Integer.parseInt(temp[0]);
 		for (int i = 1; i <= numberOfNames; i++) {
 			for (int j = 0; j < names.length; j++) {
 				if (temp[i].equals(names[j])) {
-					result ^= 1 << j;
+					result ^= 1 << j; // 해당 자리의 사람을 0으로 바꿈
 				}
 			}
 		}
-//		System.out.println(result);
-//		System.out.println(~result);
-		return result;
+		// System.out.println(result);
+		// System.out.println(~result);
+		return result; // 첫번째 두번째 사람이 먹을 수 있으면 1100
 	}
 
 }
