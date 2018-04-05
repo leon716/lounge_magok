92162702297160abf6d151ed843eddbc9a56a3e4
ImJisu-PC\ImJisu <jisu@lgcns.com>
2018-04-05 18:04:24 +0900

    commit

--- 5일차/NERDS.java
+++ 5일차/NERDS.java
@@ -1,12 +1,12 @@
 import java.awt.Point;
-import java.awt.Polygon;
-import java.awt.geom.Area;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
+
+import javax.sound.sampled.Line;
 
 public class NERDS {
 
@@ -17,30 +17,46 @@ public class NERDS {
 		int T = Integer.parseInt(br.readLine());
 		for (int t = 0; t < T; t++) {
 			int N = Integer.parseInt(br.readLine());
-			Polygon nerd = new Polygon();
-			Polygon other = new Polygon();
 			List<Point> nerds = new ArrayList<>();
+			List<Point> others = new ArrayList<>();
 			for (int n = 0; n < N; n++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				if (Integer.parseInt(st.nextToken())==1) {
-					nerd.addPoint(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+					nerds.add( new Point( Integer.parseInt( st.nextToken()), Integer.parseInt(st.nextToken() ) ) );
 
 				} else {
-					other.addPoint(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+					others.add( new Point( Integer.parseInt( st.nextToken()), Integer.parseInt(st.nextToken() ) ) );
 				}
 			}
-			nerd.
-			Area nerdArea = new Area(nerd);
-			Area otherArea = new Area(other);
-			if(nerdArea.) {
-				System.out.println("THEORY IS INVALID");
-
-			} else {
-				System.out.println("THEORY HOLDS");
-			}
+//			if(nerdArea) {
+//				System.out.println("THEORY IS INVALID");
+//
+//			} else {
+//				System.out.println("THEORY HOLDS");
+//			}
 		}
 
 	}
-	private static boolean isCrossed()
+
+	private static boolean isCrossed() {
+		
+		return false;
+		
+	}
+	
+	private static List<Point> findConvexHull(List<Point> dots){
+		List<Point> convexHull = new ArrayList<>();
+		
+		
+		
+		return convexHull;
+	}
+	
+	private static double findAngle(Point startingPoing, Point mostLeftPoint, Point compareingPoint) {
+		
+		
+		return 0;
+		
+	}
 
 }
